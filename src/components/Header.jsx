d8d93fce648ed8d087f969f37c7b87fae122a4df
Leandro <play3rtox1c@gmail.com>
2021-10-18 10:51:46 -0300

    add mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuActive: false
+    }
+  }
+
+  activeButton = () => {
+    this.setState((after) => ({ menuActive: !after.menuActive }))
+  }
+
   render() {
+    const { menuActive } = this.state;
     return (
-      <header>
-        <div className="logo-container"></div>
+      <>
+      <header className={`${ menuActive ? 'active' : 'header'}`}>
         <nav>
           <Link to="/"><h1>Home</h1></Link>
           <Link to="/about"><h1>About</h1></Link>
@@ -13,6 +27,10 @@ class Header extends Component {
           <Link to="/projects"><h1>Projects</h1></Link>
         </nav>
       </header>
+      <div onClick={ this.activeButton } className="menu">
+        <FontAwesomeIcon icon={ faBars } />
+      </div>
+      </>
     );
   }
 }
