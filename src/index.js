3ab084c5dcdc81d533cc0b231e9975db149184f1
Leandro <play3rtox1c@gmail.com>
2022-01-14 15:40:24 -0300

    arruma erro de git

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './css/index.css';
 import './css/header.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </ BrowserRouter>
+    </ HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
