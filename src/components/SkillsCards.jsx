cef8007b34e2db9df809d8daeb799be1c9a6dbc9
Leandro Henrique <play3rtox1c@gmail.com>
2022-02-12 13:18:42 -0300

    update: arrumando importação errada

--- src/components/SkillsCards.jsx
+++ src/components/SkillsCards.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react';
 import SkillCard from './SkillCard.jsx';
 import data from '../data.js';
 
